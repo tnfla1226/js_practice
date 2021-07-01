a0073572056e1d25ce06e9f6d83b834b51802f6d
tnfladl1226 <tnfladl1226@naver.com>
2021-07-01 11:48:25 +0900

    [21.07.01] #Javascript 조건문

---
+++ 1. core-js/4. 제어문/1. 조건문/1. if-else.js
@@ -0,0 +1,15 @@
+
+
+var point = 100;
+console.log(`점수: ${point}`);
+
+
+// if의 코드나 else의 코드가 한줄이면 ()생략 가능
+if (point >= 60) {
+    console.log('시험에 합격했습니다.');
+    console.log('축하합니다!');
+}
+else {
+    console.log('시험에 불합격했습니다.');
+    console.log('수고하셨습니다.');
+}
